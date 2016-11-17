6cf1a228f0b325d996de233cea8d4e8ffc4a5c20
Tyler Francis <Linkz57@gmail.com>
2016-11-17 09:42:03 -0600

    Ruining Kyle's script to watch Nagios for me
    The idea is, this mailmon script (which I've taken to calling drhelpful.sh) watches incoming mail for Nagios to complain about a server I have protected by my SBS. Once it finds something, it'll email the other recipients of that bad news and include helpful reminders about the particulars of restoring each server. Hopefully these servers die so infrequently, that I'll forget each hoop to jump through in restoring them. For me and future IT folk, Dr. Helpful will offer suggestions specific to each server, and link to an internal document containing a more verbose description of the SBS and how our particular implementation is structured, where the bits are physically stored, various addresses and what-not. 
    
    I've since expanded that scope a little, to also offer suggestions on what to do if various other machines die. Maybe later I'll include alerts about low disk space and what can be deleted first.

--- mailmon.sh
+++ mailmon.sh
@@ -5,50 +5,130 @@
 # This bash script was cobbled together quickly with no thought to how anyone else would use it so its not the best.
 # I rewrote this in python which is much easier to work with if you know python. See mailmon2.py
 
+## Kyle originally wrote a very efficient and elegant script,
+## which I have now stolen, modified, and bloated out to listen to Nagios.
+## modifications made by Tyler Francis on 2016-10-25
+# mod version 1.5
+
+## Identify yourself to an aging IT department
+SCRIPTPATH=`pwd -P`/drhelpful.sh
+hostname=`hostname`
+username=`whoami`
 
 #Path to where the mail.txt is being written
-dPATH=/home/mailmon/data
+dPATH=~
 
-#The username receiving the emails
-user_name="mailmon"
-
-
+## To increase readability and reduce size, here's a bunch of words I would normally have to repeat in most email conditions.
+usualSteps="\n -Open the Hyper-V Manager MMC Snap-In and connect to OMITTED\n -Right-click the machine you want to resurrect, and select Settings...\n -Under IDE Controller 0 click Browse... and replace the VHD with the newest VHD as created by Disk2VHD\n -Under IDE Controller 1 click Browse... and replace the VHD with the second-newest VHD as created by UrBackup. Depending on when this machine died, urBackup might have been in the middle of its backup. Don't use the latest one, it might be unfinished. Use the penultimate VHD instead\n -Change its Network Adapter to OMITTED\n -click OK, and then Start on the right\n -Consider logging into the server once it's booted to disable and stop the UrBackup client service from services.msc and the disk2vhd task from the Windows Task Scheduler"
 
 
+## clean up in case an old session didn't clean up after itself
+rm -f $dPATH/out.txt
+rm -f $dPATH/mail.txt
 
 
-from=$(cat $dPATH/mail.txt | grep From: | grep -v Message-ID | grep -v Return-Path:| egrep "[a-zA-Z0-9_.-]+@([a-zA-Z0-9-]+\.){1,}([a-zA-Z]){2,4}" -o|grep -v $user_name)
+## manually check for new email
+fetchmail -d0 -k pop.gmail.com > /dev/null
 
-if [ -z "$from" ]
+
+## copy mail to temporary working area
+cp /var/mail/$username $dPATH/mail.txt
+
+
+## don't waste my time if you haven't received any email
+if [[ $(find $dPATH/mail.txt -type f -size +2c 2>/dev/null) ]]
 then
+	## not empty
+	for line in `cat $dPATH/mail.txt`
+	do
+		## Find out who sent the message so you can reply to them, but make sure you're not mailing the SMTP server's postmaster
+		from=$(cat $dPATH/mail.txt | grep From: | grep -v Message-ID | grep -v Return-Path:| egrep "[a-zA-Z0-9_.-]+@([a-zA-Z0-9-]+\.){1,}([a-z]){2,4}" -o|grep -v "mahgmailusername\|googlemail.com\|internaldomainname.tld")
+		case $line in
+
+			uptime|Uptime )
+			UP=$(uptime | egrep "([0-9][0-9]:?){3}.up.[0-9]{1,4}.days?" -o)
+			echo $UP > $dPATH/out.txt
+			;;
+
+			Nagios ) if cat $dPATH/mail.txt | grep "Notification Type: PROBLEM"; then
+				## Nagios just sent an email about a problem.
+				## Let's find out which server died, so we can give more specific advice.
+				## TODO: throw some pipes into the above CASE-and-GREP business to include other alerts.
+				for deadservers in `cat $dPATH/mail.txt`
+				do
+					case $deadservers in
+
+						ONESERVER|oneserver ) printf "oneserver is down! \n\nIn addition to the normal Hyper-V faffery, you'll have to:\n -Boot up the oneserver VM\n -do some other stuff\n -and more still" > $dPATH/out.txt
+						;;
+
+						TWOSERVER|twoserver|TwoServer|RedServer|redserver|Redserver|REDSERVER ) printf "Ach! A server is down! There are no unusual steps involved in starting the backup VM. Only the usual steps like: $usualSteps" > $dPATH/out.txt
+						;;
+
+						Blueserver|blueserver|BlueServer|BLUESERVER ) printf "BlueServer is down!\n\nGood news : there are no unusual steps involved in starting the backup VM. Bad news : the fish is delish, but makes a small dish. \n\nHere's the usual backup VM startup procedure, if you want it: $usualSteps" > $dPATH/out.txt
+						;;
+
+						NEDSERVER|nedserver|Nedserver|NedServer ) printf "NedServer is down!\n\nGood news : there are no unusual steps involved in starting the backup VM. Bad news : He doesn't like his little bed. \n\nAnyway, if you want the usual VM startup steps, here you go: $usualSteps" > $dPATH/out.txt
+						;;
+
+						BedServer|bedserver|BEDSERVER ) printf "BedServer is down!\n\nThe BedServer VM's OS works fine and will boot right up if you follow the usual steps, but some other stuff won't work" > $dPATH/out.txt
+						;;
+
+						ThreeServer|threeserver|THREESERVER ) printf "ThreeServer is down!\n\nThis is just off-the-cuff musings, here, I don't know if it'll work." > $dPATH/out.txt
+						;;
+
+						FourServer ) printf "An important VM is down!\n\nIt should be fine, but just in case..." > $dPATH/out.txt
+						;;
+
+						foo|bar ) printf "foobar is down!\n\n    \n\noh well" > $dPATH/out.txt
+						;;
+
+					esac
+				done
+			else
+				#echo "must not be a problem" > /dev/null
+			fi
+			;;
+
+			"hi" ) echo "Why hello to you too" > $dPATH/out.txt
+			;;
+
+			esac
+	done
+
+	## mail me your variables for diagnostic purposes
+#	echo "$username" >> $dPATH/out.txt
+#	echo "$dPATH" >> $dPATH/out.txt
+
+
+
+	## add some space at the end, before reminding a forgetful IT department who and where you are, in case they want to edit or end you.
+	if [[ $(find $dPATH/out.txt -type f -size +2c 2>/dev/null) ]]
+	then
+		echo ""  >> $dPATH/out.txt
+		echo ""  >> $dPATH/out.txt
+		echo "This message has been sent by $SCRIPTPATH running on $hostname" >> $dPATH/out.txt
+
+		## Actually mail out the work done above.
+		mail -s "Robo Reply" tyler.francis@jelec.com $from < $dPATH/out.txt
+
+	fi
+
+	## clean up after yourself.
+	rm -f $dPATH/out.txt
+	rm -f $dPATH/mail.txt
+
+	## When you've read an email with GNU Mailutils (at least in version 2.99.99) it will remove that block of text from /var/mail/username and append it to ~/mbox which I find neat. Let's do the same here.
+	cat /var/mail/$username >> ~/mbox
+	echo "" > /var/mail/$username
+#	echo done
+	exit 0
+else
+#	echo "no mail received"
 	exit 0
 fi
 
-for line in `cat $dPATH/mail.txt`
-do
-	case $line in
-
-		uptime|Uptime ) 
-		UP=$(uptime | egrep "([0-9][0-9]:?){3}.up.[0-9]{1,4}.(days)?" -o)
-		echo $UP > $dPATH/out.txt
-		;;
-
-		hi|Hi|hello|Hello|sup|Sup|hola|Hola|hai|Hai|hey|Hey ) echo "Hai thar!" > $dPATH/out.txt 
-		;;
-		
-
-		help|Help ) echo -e "The following commands are accepted by StatusBot:\n\n uptime help. Case DOES matter, only first char capitalization is acceptable." > $dPATH/out.txt
-		;;
-
-		* ) echo -e "Unknown command, try again.\nFor a list of available commands, send the word \"help\" (no quotes)" > $dPATH/out.txt
-		;;
-
-		esac
-done
 
 
-mail $from < $dPATH/out.txt
-#echo $from >> $dPATH/debug.txt
-rm -f $dPATH/out.txt
-rm -f $dPATH/mail.txt
-exit 0
+## I configured Exim4 to recieve mail with fetchmail
+## and I configured fetchmail to recieve mail from Gmail using this guide:
+## https://www.axllent.org/docs/view/gmail-pop3-with-fetchmail/
